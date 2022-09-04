731046861a090465dc63249756fdc19058c49d6c
Fábio Luz <fabioluzduartefilho@alu.ufc.br>
2022-09-04 16:21:24 -0300

    module name "serverthread" changed to "scannerthread"

---
+++ src/ufcqx/scannerthread/ClientThreadScanner.java
@@ -0,0 +1,45 @@
+package src.ufcqx.scannerthread;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientThreadScanner {
+	
+	//Creating socket and out-stream
+	private Socket client;
+	private PrintStream out;
+	
+	public ClientThreadScanner() {
+		try {
+			//Starting the socket
+			this.client = new Socket("172.18.102.141", ServerThreadScanner.port);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendMessage(String message) {
+		try {
+			out = new PrintStream(this.client.getOutputStream());
+			out.println(message);
+			out.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void closeConnection() {
+		if(this.out!=null)
+			this.out.close();
+	}
+	
+	public static void main(String[] args) {
+		ClientThreadScanner client = new ClientThreadScanner();
+		client.sendMessage("Eae boy");
+	}
+
+}
---
+++ src/ufcqx/scannerthread/ServerThreadScanner.java
@@ -0,0 +1,35 @@
+package src.ufcqx.scannerthread;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerThreadScanner {
+        
+    //Setting default port
+    public final static int port = 3345;
+    
+    public static void main(String[] args) {
+
+        int clientNumbers = 0;
+        
+        try {
+            //Starting the server
+            ServerSocket server = new ServerSocket(ServerThreadScanner.port);
+            System.out.println("Server started on port: " + ServerThreadScanner.port);
+            
+            while(true) {
+                Socket client = server.accept();
+                clientNumbers++;
+                System.out.println("Client connected with IP: " + client.getInetAddress().getHostAddress());
+                new ServerWorkerThreadScanner(client, clientNumbers).start();
+            }
+            
+            //server.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
